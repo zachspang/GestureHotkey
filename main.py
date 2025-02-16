20f2a972f2e711b9d9e5b54b8e3a31e56c828f1c
zachspang <spangzach@gmail.com>
2025-02-16 15:05:10 -0800

    Add FPS counter and clean up

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+import time
 from ultralytics import YOLO
 # Load a pretrained YOLO model trained on HaGRID
 gesture_model = YOLO("models\YOLOv10n_gestures.pt")
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -13,33 +15,32 @@ with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
+    while cap.isOpened():   
+        fps_start_time = time.time()
         success, image = cap.read()
         if not success:
             print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
             continue
 
-        # To improve performance, optionally mark the image as not writeable to pass by reference.
+        # To improve performance, mark the image as not writeable to pass by reference.
         image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
-
-        # Draw the hand annotations on the image.
+        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
         #If there are hands in the frame
         if results.multi_hand_landmarks:
-            gesture_result = gesture_model(image)[0]
+            gesture_result = gesture_model(image, verbose=False)[0]
             image = gesture_result.plot()
 
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
+            #Uncomment to draw hand landmarks
+            # for hand_landmarks in results.multi_hand_landmarks:
+            #     mp_drawing.draw_landmarks(
+            #     image,
+            #     hand_landmarks,
+            #     mp_hands.HAND_CONNECTIONS,
+            #     mp_drawing_styles.get_default_hand_landmarks_style(),
+            #     mp_drawing_styles.get_default_hand_connections_style())
+        cv2.putText(image, ("FPS: " + str(round((1.0 / (time.time() - fps_start_time)), 2))), (0,40), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 1, cv2.LINE_8)
         cv2.imshow('Annotated Webcam', image)
 
         if cv2.waitKey(5) & 0xFF == 27:
