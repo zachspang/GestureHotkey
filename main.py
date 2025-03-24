bdd381e220198617b1351e60f6c9862894906914
zachspang <90374182+zachspang@users.noreply.github.com>
2025-03-24 15:51:42 -0700

    Start tk on main thread to fix a crash when loading configs

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from threading import *
 from gui import gui_window
 from detection import detection_window
 
-t1 = Thread(target=gui_window)
-t2 = Thread(target=detection_window)
-t2.daemon = True
+detection_thread = Thread(target=detection_window)
+detection_thread.daemon = True
 
-t1.start()
-t2.start()
+detection_thread.start()
+gui_window()
